81b1368ec9a84a6d955ed07981ca808b175d1cd6
Puma Thongsithavong <v-photho@microsoft.com>
2020-05-11 12:59:57 -0700

    Commiting build folder.

---
+++ build/precache-manifest.6f80fc9a3ee3d68da80eefc5ac22db63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e7533ec13c8126de9c16a56ee30c98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333ff35e6dcec01be07a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "096d1bc45f47d25474ac",
+    "url": "/static/js/2.06d79e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06d79e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333ff35e6dcec01be07a",
+    "url": "/static/js/main.5f9d476b.chunk.js"
+  },
+  {
+    "revision": "68ec54236f2f5500ee82",
+    "url": "/static/js/runtime-main.1db87b7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
